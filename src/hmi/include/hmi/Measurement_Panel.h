f19f126683c2f5b2f4e417353f287a422973cc9d
Kevin Eppacher <kevin-eppacher@hotmail.de>
2024-07-31 12:58:36 +0000

    Publihsher HMI

--- src/hmi/include/hmi/Measurement_Panel.h
+++ src/hmi/include/hmi/Measurement_Panel.h
@@ -2,20 +2,13 @@
 #define MEASUREMENT_PANEL_H
 
 #include "imgui.h"
-#include <vector>
-#include <string>
-#include <geometry_msgs/Pose.h>
-#include <geometry_msgs/PoseArray.h>
 
 class MeasurementPanel {
 public:
     MeasurementPanel();
     void render();
-    void addPose(const geometry_msgs::Pose& pose);
-    void updatePose(int index, const geometry_msgs::Pose& pose);
 private:
-    geometry_msgs::PoseArray poses;
-    int selectedPoseIndex;
+    // Add private members and methods if needed
 };
 
 #endif // MEASUREMENT_PANEL_H
